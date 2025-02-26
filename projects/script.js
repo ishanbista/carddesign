101cd50d45222d3c8ff76fb1c5b2e09c674377aa
Ishan Bista <125456778+ishanbista@users.noreply.github.com>
2025-02-26 16:37:00 +0545

    Add files via upload

---
+++ projects/script.js
@@ -0,0 +1,17 @@
+
+// let gameNumber=25;
+// n=prompt("guess a number");
+// if(n == gameNumber)
+// {
+//     console.log("you guessed correct number");
+// }
+// else{
+//     console.log("you guessed incorrect number");
+// }
+// let gameNumber=25;
+// n=prompt("guesss a number");
+// while(n != gameNumber)
+// {
+//     n=prompt("guess again");
+// }
+// console.log("you guessed correct number");
